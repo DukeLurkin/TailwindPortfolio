683879b75a691407c22e1550ed1709ee0b116dee
DukeLurkin <durkin.lukas@gmail.com>
2025-08-18 12:06:00 -0400

    first commit, installed and updated all tailwind npm & vita, created navbar for desktop, added background image

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import "./index.css";
+
+const App = () => {
+  return (
+    <main className="overflow-x-hidden bg-black tracking-tighter text-gray-200 antialiased"> 
+      <Navbar />
+      <HeroSection/>
+    </main>
+      
+  )
+}
+
+export default App
