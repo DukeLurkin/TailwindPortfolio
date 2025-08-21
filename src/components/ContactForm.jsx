e772c321f39e4e0ea9517c0a4fbfa09e7eac5bcc
DukeLurkin <durkin.lukas@gmail.com>
2025-08-21 17:02:41 -0400

    added motion effects to the award/achievements, testimonials, contact and footer sections

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import emailjs from "@emailjs/browser"
 import { Toaster, toast } from "react-hot-toast";
+import {motion} from "framer-motion"
 
 
 const ContactForm = () => {
@@ -71,7 +72,11 @@ const ContactForm = () => {
         <h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">
             Let's Connect
         </h2>
-        <form onSubmit={handleSubmit}>
+        <motion.form 
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{duration: 0.8, delay: 1}}
+            onSubmit={handleSubmit}>
             <div className="mb-4">
                 <input type="text"
                     id="name"
@@ -81,7 +86,11 @@ const ContactForm = () => {
                     onChange={handleChange}
                     className="mb-8 w-full appearance-none rounded-lg border border-gray-900 bg-transparent px-3 py-2 text-sm focus:border-gray-400 focus:outline-none" />
                     {errors.name && (
-                        <p className="text-sm text-pink-700">{errors.name}</p>
+                        <motion.p
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        aria-live="polite"
+                        className="text-sm text-pink-700">{errors.name}</motion.p>
                     )}
             </div>
             <div className="mb-4">
@@ -93,7 +102,11 @@ const ContactForm = () => {
                     onChange={handleChange}
                     className="mb-8 w-full appearance-none rounded-lg border border-gray-900 bg-transparent px-3 py-2 text-sm focus:border-gray-400 focus:outline-none" />
                     {errors.email && (
-                        <p className="text-sm text-pink-700">{errors.email}</p>
+                        <motion.p 
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        aria-live="polite"
+                        className="text-sm text-pink-700">{errors.email}</motion.p>
                     )}
             </div>
             <div className="mb-4">
@@ -105,13 +118,17 @@ const ContactForm = () => {
                     onChange={handleChange}
                     className="mb-8 w-full appearance-none rounded-lg border border-gray-900 bg-transparent px-3 py-2 text-sm focus:border-gray-400 focus:outline-none" rows="4" />
                     {errors.message && (
-                        <p className="text-sm text-pink-700">{errors.message}</p>
+                        <motion.p
+                        initial={{opacity: 0}}
+                        whileInView={{opacity: 1}}
+                        aria-live="polite" 
+                        className="text-sm text-pink-700">{errors.message}</motion.p>
                     )}
             </div>
             <button type="submit" className={'mb-8 w-full rounded bg-yellow-400 px-4 py-2 text-sm font-semibold text-slate-950 hover:bg-yellow-500 $ { isSending ? "cursor-not-allowed opacity-50" : "" }'} disabled={isSending}>
                     {isSending ? "Sending..." : "Send"}
             </button>
-        </form>
+        </motion.form>
     </div>
   )
 }
